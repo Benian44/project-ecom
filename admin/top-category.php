1ffabbcd96492f73d386ff0629627b3b5f1146f6
Benian44 <155669009+Benian44@users.noreply.github.com>
2025-05-24 11:23:40 +0000

    Add files via upload

---
+++ admin/top-category.php
@@ -0,0 +1,89 @@
+<?php require_once('header.php'); ?>
+
+<section class="content-header">
+	<div class="content-header-left">
+		<h1>Voir les catégories principales</h1>
+	</div>
+	<div class="content-header-right">
+		<a href="top-category-add.php" class="btn btn-primary btn-sm">Ajouter un nouveau</a>
+	</div>
+</section>
+
+
+<section class="content">
+
+  <div class="row">
+    <div class="col-md-12">
+
+
+      <div class="box box-info">
+        
+        <div class="box-body table-responsive">
+          <table id="example1" class="table table-bordered table-hover table-striped">
+			<thead>
+			    <tr>
+			        <th>#</th>
+			        <th>Nom de la catégorie principale</th>
+                    <th>Afficher dans le menu ?</th>
+			        <th>Action</th>
+			    </tr>
+			</thead>
+            <tbody>
+            	<?php
+            	$i=0;
+            	$statement = $pdo->prepare("SELECT * FROM tbl_top_category ORDER BY tcat_id DESC");
+            	$statement->execute();
+            	$result = $statement->fetchAll(PDO::FETCH_ASSOC);							
+            	foreach ($result as $row) {
+            		$i++;
+            		?>
+					<tr>
+	                    <td><?php echo $i; ?></td>
+	                    <td><?php echo $row['tcat_name']; ?></td>
+                        <td>
+                            <?php 
+                                if($row['show_on_menu'] == 1) {
+                                    echo 'Oui';
+                                } else {
+                                    echo 'Non';
+                                }
+                            ?>
+                        </td>
+	                    <td>
+	                        <a href="top-category-edit.php?id=<?php echo $row['tcat_id']; ?>" class="btn btn-primary btn-xs">Modifier</a>
+	                        <a href="#" class="btn btn-danger btn-xs" data-href="top-category-delete.php?id=<?php echo $row['tcat_id']; ?>" data-toggle="modal" data-target="#confirm-delete">Supprimer</a>
+	                    </td>
+	                </tr>
+            		<?php
+            	}
+            	?>
+            </tbody>
+          </table>
+        </div>
+      </div>
+  
+
+</section>
+
+
+<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="myModalLabel">Confirmation de suppression</h4>
+            </div>
+            <div class="modal-body">
+                <p>Êtes-vous sûr de vouloir supprimer cet article ?</p>
+                <p style="color:red;">Attention ! Tous les produits, les catégories de niveau intermédiaire et les catégories de niveau inférieur sous cette catégorie principale seront supprimés de toutes les tables telles que la table des commandes, la table des paiements, la table des tailles, la table des couleurs, la table des évaluations, etc.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                <a class="btn btn-danger btn-ok">Supprimer</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php require_once('footer.php'); ?>
